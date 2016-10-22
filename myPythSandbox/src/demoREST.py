3ceee16eb053d33aa2b03c1c87edc1a88428ab20
wawing <wes.wing@gmail.com>
2016-10-22 12:33:21 -0400

    print json key and values

--- myPythSandbox/src/demoREST.py
+++ myPythSandbox/src/demoREST.py
@@ -9,9 +9,8 @@ REST response
 
 @author: ww
 '''
-
 import requests
-
+import json
 
 resp = requests.get('http://services.groupkt.com/country/get/all')
 print resp.status_code;
@@ -20,8 +19,28 @@ if resp.status_code != 200:
     # This means something went wrong.
     #raise errors.ApiError('GET /tasks/ {}'.format(resp.status_code))
     raise NameError("GET error detect")
-
+'''
 for todo_item in resp.json():
-    print('{} {}'.format(todo_item('name') ))
-    
-    #print resp.json();
+    #print('{} {}'.format(todo_item('name') ))
+'''
+print resp;  
+print resp.json();
+print resp.text;
+
+# change the JSON string into a JSON object
+jsonObject = json.loads(resp.text);
+# print the keys and values
+for key in jsonObject:
+    value = jsonObject[key]
+    print("The key and value are ({}) = ({})".format(key, value) )
+
+for majorkey, subdict in jsonObject.iteritems():
+    print "major Key is " + majorkey
+    for subkey, value in subdict.iteritems():
+            print "subkey is " + subkey, value
+
+j = json.loads(resp.text);  #converts to dictionary
+for x in j['RestResponse']['result']:
+    print x['name']
+
+pass
